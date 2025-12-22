85e62eec031a0c57bc4961295c035c8e9cb13a9a
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-22 06:34:40 -0800

    Update local_models.py

--- local_models.py
+++ local_models.py
@@ -1,1 +1,16 @@
+from langchain_ollama import ChatOllama
 
+chat = CharOllama(
+    mode="deepseek-r1:1.5b",
+    tempature=0,
+)
+
+messages = [
+    (
+        "system",
+        "You are a helpful assistant. ",
+    ),
+    ("human", "What makes LangChain great for working with LLMs?"),
+]
+ai_msg = chat.invoke(messages)
+print(ai_msg.content)
