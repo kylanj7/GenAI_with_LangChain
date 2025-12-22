a57089f7164177f58dae6064848d6d5105c96f29
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-22 06:40:05 -0800

    Update local_models.py

--- local_models.py
+++ local_models.py
@@ -1,3 +1,4 @@
+## Ollama
 from langchain_ollama import ChatOllama
 
 chat = CharOllama(
@@ -13,4 +14,30 @@ messages = [
     ("human", "What makes LangChain great for working with LLMs?"),
 ]
 ai_msg = chat.invoke(messages)
+print(ai_msg.content)
+
+## Huggingface
+
+From langchain_core.messages import SystemMessage, HumanMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+
+llm = HuggingFacePipeline.from_model_id(
+    model_id="TinyLlama/TinyLlama-1.1b-Chat-v1.0",
+    task="text-generation",
+    pipeline_kwargs=dict(
+        max_new_tokens=512,
+        do_sample=False,
+        repetition_penalty=1.03,
+    ),
+)
+
+chat_model = ChatHuggingFace(llm=llm)
+
+messages = [
+    systemMessage(content="You're a helpful assistant"),
+    HumanMessage(
+        content="Explain the concept of machine learning in simple terms"
+    ),
+]
+ai_msg = chat.model.invoke(messages)
 print(ai_msg.content)
