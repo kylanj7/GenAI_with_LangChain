9088ae9779c13820a7102d44ebdccecce2ac3d7f
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-25 08:18:25 -0800

    Update Error_handing.py

--- Error_handing.py
+++ Error_handing.py
@@ -1,6 +1,11 @@
+
+import logging
 from enum import Enum
+from lanchain_core.language_core.language_models import GenericFakeChatModel
+from langchain_core.messages import AIMessage
 from langchain.output_parsers import EnumOutputParser
 from langchain_google_vertexai import ChatVertexAI
+
 
 class IsSuitableJobEnum(Enum):
     YES = "YES"
@@ -9,3 +14,20 @@ class IsSuitableJobEnum(Enum):
 parser = EnumOutputParser(enum=IsSuitableJobEnum)
     "Given a job description, decide whether it suites a junior Java developer."
     "\nJOB DESCRIPTION:\n{job_description}\n\nAnswer only YES or NO."
+
+job_description = "test job description"
+
+llm = ChatVertexAI(model="gemini-2.0-flash")
+analyze_chain = llm | parser
+
+logger = logging.getlogger(__name__)
+
+def analyze_job_description(state):
+    try:
+      prompt = prompt_template_enum.format(job_description=job__description)
+      result = analyze_chain.invoke(prompt)
+      return {"is_suitable": result)
+    except Exception s e:
+      logger.error(f"Exception {e} occured while executing analyze_job_description")
+      return {is_suitable": False
+    
