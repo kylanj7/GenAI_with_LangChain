ad03cd608293cc8074fa1dc3dd0268c4c99d8ad9
Kylan Michael Johnson <103906447+kylanj7@users.noreply.github.com>
2025-12-28 20:37:46 -0800

    Update Error_handing.py

--- Error_handing.py
+++ Error_handing.py
@@ -9,6 +9,7 @@ from typing_extensions import TypedDict
 from typing import Annotated, Literal
 from operator import add
 from langgraph.graph import StateGraph, START, END
+from langgraph.pregel import RetryPolicy
 from langchain_core.runnables.config import RunnableConfig
 
 class IsSuitableJobEnum(Enum):
@@ -95,9 +96,50 @@ display(Image(graph.get_graph().draw_mermaid_png()
 res = graph.invoke({job_description":"fake_jd"}, config={"configurable": {model_provider": "fake"}})
 print(res)
 
-   
+res = graph.invoke({"job_description": job_description}, config={"configurable": {"model_provider": "fake"}})
+print(res)
 
-              
+res = graph.invoke({"job_description":"fake_jd"}, config={"configurable": {"model_provider": "fake"}})
+print(res)
+
+fake_llm.invoke("test")
+
+chain_fallback = RunnableLambda(lambda _: print("running fallback"))
+chain = fake_llm | RunnableLambda(lambda _: print("running main chain"))
+chain_with_fb = chain.with_fallbacks([chain_fallback])
+
+chain_with_fb.invoke("test")
+chain_with_fb.invoke("test")
+
+fake_llm_retry = fake_llm.with_retry(
+    retry_if_exception_type=(ValueError,),
+    wait_exponential_jitter=True,
+    stop_after_attept=2,
+)
+
+analyze_chain_fake_retries.invoke("test")
+
+def analyze_job_description(state, config: RunnableConfig):
+    model_provider = config["configurable"].get("model_provider", "Google")
+    llm = llms[model_provider]
+    analyze_chain = llm | parser
+    prompt = prompt_template_enum.format(job_description=job_description)
+    result = analyze_chain.invoke(prompt)
+    return {is_suitable": result}
+
+builder = StateGraph(JobApplicationState)
+builder.add_node("analyze_job_description", analyze_job_description, retry=RetryPolicy(retry_on=ValueError, max_attempts=2))
+builder.add_node("generate_application", generate_application)
+builder.add_edge(START, "analyze_job_description")
+builder.add_conditional_edges(
+    "analyze_job_description", is_suitable_condition)
+builder.add_edge("generate_application", END)
+
+graph = builder.compile()
+
+from IPython.display import Image, display
+display(Image(graph.get_graph().graw_mermaid_png()))
+
 
 
 
